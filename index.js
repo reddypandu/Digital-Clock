ba68e3f0286fc89114aa1cdbec2feae78d2e646b
reddypandu <128275448+reddypandu@users.noreply.github.com>
2023-03-18 23:22:13 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+let hourE1 = document.getElementById("hour");
+let minuteE1 = document.getElementById("minutes");
+let secondE1 = document.getElementById("seconds");
+let ampmE1 = document.getElementById("ampm");
+
+function updateClock() {
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    let ampm = "AM";
+
+    if(h > 12) {
+        h = h - 12;
+        ampm = "PM";
+    }
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+
+    hourE1.innerText = h;
+    minuteE1.innerText = m;
+    secondE1.innerText = s;
+    ampmE1,(innerText = ampm);
+    setTimeout(()=>{
+        updateClock()
+    }, 1000)
+}
+updateClock()
